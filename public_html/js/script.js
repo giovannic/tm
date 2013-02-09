2e4b7928b88be9d19ac41992d387ec796b086e4b
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-02-09 16:32:58 +0000

    nav works now

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+//initialise some bootstrap javascript
 	$('.dropdown-toggle').dropdown();
-	$('.carousel').carousel();
+	$('.carousel').carousel('pause');
+
+
+	$('.option-select').click(function() {
+		$('.option-select').removeClass('active');
+		$(this).addClass('active');
+
+		$('#option-carousel').carousel(parseInt($(this).attr("index")));
+
+		
+
+	});
+
+
+
 })
