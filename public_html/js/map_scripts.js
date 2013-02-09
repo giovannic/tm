8c32306e7588113df738ebfae649731ba2b4b84b
Ben Davis <imben123@gmail.com>
2013-02-09 14:12:59 +0000

    making sure files are added

---
+++ public_html/js/map_scripts.js
@@ -0,0 +1,8 @@
+function initialiseMap() {
+	var mapOptions = {
+		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
