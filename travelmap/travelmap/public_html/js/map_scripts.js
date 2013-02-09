b07a6a43f5b16737bb947c7ea56c366d27491a23
Ben Davis <imben123@gmail.com>
2013-02-09 17:07:43 +0000

    city list

--- travelmap/travelmap/public_html/js/map_scripts.js
+++ travelmap/travelmap/public_html/js/map_scripts.js
@@ -1,8 +1,14 @@
 var map;
 var xmlreq;
+var xmlreq2;
 var markers;
 var cities;
-var geocoder;
+
+function log(msg) {
+    setTimeout(function() {
+        throw new Error(msg);
+    }, 0);
+}
 
 function initialiseMap() {
 
@@ -16,7 +22,7 @@ function initialiseMap() {
 	};
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-	geocoder = new google.maps.Geocoder();
+	markers = new Array();
 
 	xmlreq = new XMLHttpRequest();
 	var fileLocation = getBaseURL() + "resources/city_list.txt";
@@ -24,7 +30,11 @@ function initialiseMap() {
 	xmlreq.onreadystatechange = recieveCities;
 	xmlreq.send();
 
-	markers = new Array();
+	xmlreq2 = new XMLHttpRequest();
+	fileLocation = getBaseURL() + "resources/city_longlats.txt";
+	xmlreq2.open("GET", fileLocation, true);
+	xmlreq2.onreadystatechange = recieveLLs;
+	xmlreq2.send();
 }
 
 function recieveCities() {
@@ -39,22 +49,26 @@ function recieveCities() {
 	
 }
 
-function addMarker(results,status) {
-	if (status == google.maps.GeocoderStatus.OK) {
-		var marker = new google.maps.Marker({
-			map: map,
-			position: results[0].geometry.location,
-			title: results[0].formatted_address
-		});
-		markers.push(marker);
-
-		cities.pop();
-		geocoder.geocode({'address' : cities[cities.length - 3]}, addMarker);
-	} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {    
-		setTimeout(function() { geocoder.geocode({'address' : cities[cities.length - 3]}, addMarker); }, 200);
-	} else {
-		alert("Geocode was not successful for the following reason: " + status);
+function recieveLLs() {
+	if (xmlreq.readyState === 4) {  // Makes sure the document is ready to parse.
+		if (xmlreq.status === 200) {  // Makes sure it's found the file.
+			var allText = xmlreq.responseText;
+			cities = xmlreq.responseText.split("\n"); // Will separate each line into an array
+			
+			geocoder.geocode({'address' : cities[cities.length - 3]}, addMarker);
+		}
 	}
+	
+}
+
+function addMarker(longitude, latitude, city) {
+	var longandlat = google.maps.LatLng(latitude, longitude);
+	var marker = new google.maps.Marker({
+		map: map,
+		position: results[0].geometry.location,
+		title: results[0].formatted_address
+	});
+	markers.push(marker);
 }
 
 function getBaseURL () {
