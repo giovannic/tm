f128ca7757ffcea967602667948f73440a0ebeca
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-02-09 21:17:34 +0000

    added sliders to other two pref panes - they don't do anything yet

--- travelmap/travelmap/public_html/js/script.js
+++ travelmap/travelmap/public_html/js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 //Setup Map
 	initialiseMap();
 
-//Declare global array to store preferences in
+//ensure carousel doesn't run itself - is this my most shameless hack?
+	window.setInterval(function() {$('#option-carousel').carousel('pause')}, 1000);
 
 
 //initialise some bootstrap javascript
@@ -16,14 +17,25 @@ $(document).ready(function() {
 	});
 
 //sliders
-	$( ".slider" ).slider({
+	$( ".standard-slider" ).slider({
 		value:50,
-		min: 00,
+		min: 0,
 		max: 100,
 		slide: function( event, ui ) {
-			//console.log($(this).parent().children('p'));
 			$(this).parent().children('p').html(ui.value + '%');
 			$(this).parent().children('p').attr({"data": ui.value});
+		}
+	});
+
+	$( ".range-slider" ).slider({
+		range: true,
+		values: [ 350, 1000 ],
+		min: 150,
+		max: 1500,
+		slide: function( event, ui ) {
+			$(this).parent().children('p').html("Min: &#163;"+ui.values[0]+"<br/>Max: &#163;"+ui.values[1]);
+			$(this).parent().children('p').attr({"data-min": ui.values[0]});
+			$(this).parent().children('p').attr({"data-max": ui.value[1]});
 		}
 	});
 
@@ -39,6 +51,14 @@ $(document).ready(function() {
 			todo[$(this).children('div').attr("pref")] = $(this).children('p').attr("data");
 		});
 
+		$('#todo-prefs .tab-pane').each(function() {
+			todo[$(this).children('div').attr("pref")] = $(this).children('p').attr("data");
+		});
+
+		$('#todo-prefs .tab-pane').each(function() {
+			todo[$(this).children('div').attr("pref")] = $(this).children('p').attr("data");
+		});
+
 		console.log(todo);
 
 		allprefs[0] = type;
@@ -46,6 +66,8 @@ $(document).ready(function() {
 		allprefs[2] = todo;
 
 		console.log(allprefs);
+
+
 	})
 
 });
