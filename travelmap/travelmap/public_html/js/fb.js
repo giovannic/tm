c3f665ce78c2e54f44d810c77f7d8a7860f9bdd0
Ubuntu <ubuntu@ip-10-48-206-68.eu-west-1.compute.internal>
2013-02-10 14:51:58 +0000

    fb

--- travelmap/travelmap/public_html/js/fb.js
+++ travelmap/travelmap/public_html/js/fb.js
@@ -1,12 +1,16 @@
-
-   function login() {
+function login() {
      FB.login(function(response) {
        if (response.authResponse) {
 	 FB.api('/me', function(res) {
 	   console.log(res.name);
+	 get_fbloc();
 	 });
        } else {
          console.log("fail");
        }
      });
-   }
+}
+
+function get_fbloc() {
+  var locations = FB.api('me?fields=id,name,photos.fields(place)');
+}
